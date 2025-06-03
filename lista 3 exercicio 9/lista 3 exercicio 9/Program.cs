fe885ec5f3c0d1e7fca5fd5972535b84152b3620
dosantosednei <ednei.s@aluno.ifsp.edu.br>
2025-06-03 17:29:13 -0300

    Add files via upload

---
+++ lista 3 exercicio 9/lista 3 exercicio 9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_3_exercicio_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0;
+            int n2 = 1;
+            int soma;
+            int i;
+
+            for (i = 1; i <=30; i++)
+            {
+                soma = n1 + n2;
+                n2 = n1;
+                n1  = soma;
+
+                Console.WriteLine("a sequqencia de Fibonacci é :{0}", soma);
+
+
+            }
+                
+
+                    
+
+
+
+
+        }
+    }
+}
---
+++ lista 3 exercico 8/lista 3 exercico 8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_3_exercico_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma;
+            int i;
+            soma = 0;
+
+            for (i = 1;  i <= 100; i++)
+            {
+                soma += i;
+            }
+            Console.WriteLine("a soma dos numeros de 1 a 100 é:{0}" , soma);
+               
+        }
+    }
+}
